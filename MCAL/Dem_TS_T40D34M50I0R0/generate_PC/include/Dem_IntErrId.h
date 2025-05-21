7db67e54c5a0973f227571265c7c09e4369b9b4a
zhouzhen <1915808871@qq.com>
2025-05-21 22:54:28 +0800

    【将EB工程添加到S32工程中，解决编译错误】

--- MCAL/Dem_TS_T40D34M50I0R0/generate_PC/include/Dem_IntErrId.h
+++ MCAL/Dem_TS_T40D34M50I0R0/generate_PC/include/Dem_IntErrId.h
@@ -7,10 +7,10 @@
 *   Autosar Version      : 4.7.0
 *   Autosar Revision     : ASR_REL_4_7_REV_0000
 *   Autosar Conf.Variant :
-*   SW Version           : 3.0.0
-*   Build Version        : S32K3_RTD_3_0_0_P01_D2303_ASR_REL_4_7_REV_0000_20230331
+*   SW Version           : 5.0.0
+*   Build Version        : S32K3_RTD_5_0_0_D2408_ASR_REL_4_7_REV_0000_20241002
 *
-*   (c) Copyright 2020 - 2023 NXP Semiconductors
+*   (c) Copyright 2020 - 2024 NXP
 *   All Rights Reserved.
 *
 *   NXP Confidential. This software is owned or controlled by NXP and may only be
@@ -69,78 +69,14 @@ extern "C"{
 /*==================================================================================================
 *                                       DEFINES AND MACROS
 ==================================================================================================*/
-      
-  
-/** @brief Identifier for DEM error ADC_E_CHANNEL_ID_CORRUPTED*/
-#define DemConf_DemEventParameter_ADC_E_CHANNEL_ID_CORRUPTED   ((Dem_EventIdType) 0xd)
-
-/** @brief Identifier for DEM error ADC_E_INVALID_CONV_MODE*/
-#define DemConf_DemEventParameter_ADC_E_INVALID_CONV_MODE   ((Dem_EventIdType) 0x10)
-
-/** @brief Identifier for DEM error ADC_E_LIMIT_RANGE*/
-#define DemConf_DemEventParameter_ADC_E_LIMIT_RANGE   ((Dem_EventIdType) 0xe)
-
-/** @brief Identifier for DEM error ADC_E_TIMEOUT*/
-#define DemConf_DemEventParameter_ADC_E_TIMEOUT   ((Dem_EventIdType) 0xc)
-
-/** @brief Identifier for DEM error ADC_E_TRIG_SRC*/
-#define DemConf_DemEventParameter_ADC_E_TRIG_SRC   ((Dem_EventIdType) 0xf)
-
-/** @brief Identifier for DEM error DEM_LIN_E_TIMEOUT*/
-#define DemConf_DemEventParameter_DEM_LIN_E_TIMEOUT   ((Dem_EventIdType) 0x22b)
-
-/** @brief Identifier for DEM error DEM_MCU_E_GENERIC_ERROR*/
-#define DemConf_DemEventParameter_DEM_MCU_E_GENERIC_ERROR   ((Dem_EventIdType) 0x15)
-
-/** @brief Identifier for DEM error MCL_DMA_E_BUS*/
-#define DemConf_DemEventParameter_MCL_DMA_E_BUS   ((Dem_EventIdType) 0x11)
-
-/** @brief Identifier for DEM error MCL_DMA_E_DESCRIPTOR*/
-#define DemConf_DemEventParameter_MCL_DMA_E_DESCRIPTOR   ((Dem_EventIdType) 0x12)
-
-/** @brief Identifier for DEM error MCL_DMA_E_ECC*/
-#define DemConf_DemEventParameter_MCL_DMA_E_ECC   ((Dem_EventIdType) 0x13)
-
-/** @brief Identifier for DEM error MCL_DMA_E_PRIORITY*/
-#define DemConf_DemEventParameter_MCL_DMA_E_PRIORITY   ((Dem_EventIdType) 0x14)
-
-/** @brief Identifier for DEM error MCU_E_CLOCK_FAILURE*/
-#define DemConf_DemEventParameter_MCU_E_CLOCK_FAILURE   ((Dem_EventIdType) 0x4)
-
-/** @brief Identifier for DEM error MCU_E_DEFAULTCASE_FAILURE*/
-#define DemConf_DemEventParameter_MCU_E_DEFAULTCASE_FAILURE   ((Dem_EventIdType) 0xb)
-
-/** @brief Identifier for DEM error MCU_E_FORBIDDEN_INVOCATION*/
-#define DemConf_DemEventParameter_MCU_E_FORBIDDEN_INVOCATION   ((Dem_EventIdType) 0x9)
-
-/** @brief Identifier for DEM error MCU_E_FUNCRESET_FAILURE*/
-#define DemConf_DemEventParameter_MCU_E_FUNCRESET_FAILURE   ((Dem_EventIdType) 0x7)
-
-/** @brief Identifier for DEM error MCU_E_INVALID_PARAMETER*/
-#define DemConf_DemEventParameter_MCU_E_INVALID_PARAMETER   ((Dem_EventIdType) 0xa)
-
-/** @brief Identifier for DEM error MCU_E_MEMORY_CORRUPTION*/
-#define DemConf_DemEventParameter_MCU_E_MEMORY_CORRUPTION   ((Dem_EventIdType) 0x8)
-
-/** @brief Identifier for DEM error MCU_E_PLLLOCK_FAILURE*/
-#define DemConf_DemEventParameter_MCU_E_PLLLOCK_FAILURE   ((Dem_EventIdType) 0x5)
-
-/** @brief Identifier for DEM error MCU_E_SPURIOUSINT_FAILURE*/
-#define DemConf_DemEventParameter_MCU_E_SPURIOUSINT_FAILURE   ((Dem_EventIdType) 0x6)
-
-/** @brief Identifier for DEM error MCU_E_SWITCHMODE_FAILURE*/
-#define DemConf_DemEventParameter_MCU_E_SWITCHMODE_FAILURE   ((Dem_EventIdType) 0x16)
-
-/** @brief Identifier for DEM error MCU_E_TIMEOUT_FAILURE*/
-#define DemConf_DemEventParameter_MCU_E_TIMEOUT_FAILURE   ((Dem_EventIdType) 0x1)
-
-/** @brief Identifier for DEM error MCU_E_INVALIDFXOSC_CONFIG*/
-#define DemConf_DemEventParameter_MCU_E_INVALIDFXOSC_CONFIG   ((Dem_EventIdType) 0x2)
-
-/** @brief Identifier for DEM error MCU_E_CLOCKMUXSWITCH_FAILURE*/
-#define DemConf_DemEventParameter_MCU_E_CLOCKMUXSWITCH_FAILURE   ((Dem_EventIdType) 0x3)
-
-
+#if 0
+[!LOOP " DemConfigSet"!]      
+  [!LOOP " DemEventParameter/*"!]
+/** @brief Identifier for DEM error [!"node:name('.')"!]*/
+#define DemConf_DemEventParameter_[!"node:name('.')"!]   ((Dem_EventIdType) [!"num:inttohex(node:value('./DemEventId'))"!])
+[!ENDLOOP!]
+[!ENDLOOP!]
+#endif
 /*==================================================================================================
 *                                             ENUMS
 ==================================================================================================*/
